e63143cc2348214aea8bb15631eeb9eea2f8a9b4
nan-tokyo <782868995@qq.com>
2020-03-26 18:00:21 +0800

    再次测试

---
+++ Project5/Project5/test.c
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//void test()
+//{
+//	static int a = 10;
+//	a++;
+//	printf("%d\n", a);
+//}
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", a);
+//	int* p = &a;  //p是一个指针变量
+//	*p = 20;
+//	printf("%d", a);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d", sizeof(int*));
+//	return 0;
+//}
+//
+//创建书的类型
+//struct book
+//{
+//	char name[30];
+//	double price;
+//	char id[30];
+//};
+//int main()
+//{
+//	struct book b1 = { "慧乔的时间", 33.66, "akq124717465" };
+//	printf("书名：%s\n", b1.name);
+//	printf("定价：%lf\n", b1.price);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int age = 10;
+//	if (5 == age)
+//		printf("hehe");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	for (int i = 1; i <= 100;i++)
+//		if (i % 2 == 1)
+//			printf("该值为奇数");
+//}
+
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch(day)//switch里面的表达式必须为整型
+//	{
+//		case 1:
+//		case 2:
+//		case 3:
+//		case 4:
+//		case 5:
+//			printf("工作日\n");
+//			break;
+//		case 6:
+//		case 7:
+//			printf("休息日\n");
+//			break;
+//		default://处理case不能处理的情况，取值不在范围内
+//			printf("输入错误");
+//		break;
+//
+//	}
+//}
+
+int main()
+{
+	int i = 0;
+	while (i < 10)
+	{
+		printf("")
+	}
+
+	return 0;
+}
